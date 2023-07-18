e506650835cf7dc34e1650ca0e53325e6ca7c2b9
Alyzee Sosa <alyzeesosacollege@gmail.com>
2023-07-18 13:31:01 -0700

    Initial Commit

---
+++ unhousED_App/unhousED_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  unhousED_AppApp.swift
+//  unhousED_App
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct unhousED_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
